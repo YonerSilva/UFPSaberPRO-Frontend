b028372e22f5c51d4eae872701614cd520a1a848
Yoner Silva <yonerarbeysl@ufps.edu.co>
2022-12-03 22:44:26 -0500

    configuaciones para el servidor de produccion

--- src/app/routes/routes_constante.js
+++ src/app/routes/routes_constante.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Constantes = {
-     SISTEMA: "http://ec2-3-17-151-203.us-east-2.compute.amazonaws.com/",
+     SISTEMA: "http://ec2-13-59-140-88.us-east-2.compute.amazonaws.com:8080/",
 }
 
 export default Constantes;
